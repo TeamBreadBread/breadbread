5311d06f51973959c2cbd3c0a664c37632e10437
Wonbeen Lee <wblee800@gmail.com>
2026-03-22 03:58:33 +0000

    feat: initialize Spring Boot application with Gradle setup

---
+++ be/src/main/java/com/teambreadbread/bread_be/BreadBeApplication.java
@@ -0,0 +1,13 @@
+package com.teambreadbread.bread_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreadBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreadBeApplication.class, args);
+	}
+
+}
---
+++ be/src/test/java/com/teambreadbread/bread_be/BreadBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teambreadbread.bread_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreadBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
