b8c43ff00a830838ef9e737cdb84d31799f99b6e
Wonbin Lee <wblee800@gmail.com>
2026-03-29 23:03:19 +0900

    chore: add Prettier configuration and update package.json for formatting

--- be/eslint.config.mjs
+++ be/eslint.config.mjs
@@ -1,10 +1,11 @@
 import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
 
 export default tseslint.config(
   { ignores: ['dist', 'node_modules'] },
   {
     files: ['**/*.ts'],
-    extends: tseslint.configs.recommended,
+    extends: [tseslint.configs.recommended, prettier],
     rules: {
       'no-console': 'error',
     },
