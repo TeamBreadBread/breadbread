f27c352557686229d42a245a0d9c82ad540b59b2
jihyun1123 <janet9182@naver.com>
2026-07-01 14:19:52 +0900

    fix : AISearchResultPage requireLogin 함수 반환 수정

--- apps/fe/src/pages/AISearchResultPage.tsx
+++ apps/fe/src/pages/AISearchResultPage.tsx
@@ -381,7 +381,7 @@ export default function AISearchResultPage({ courseId, from }: AISearchResultPag
       } finally {
         setLikeBusy(false);
       }
-    })();
+    });
   };
 
   const handlePlaceClick = (place: CoursePlace) => {
